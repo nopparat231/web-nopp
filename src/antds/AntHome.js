262a75146ba10f1d4facbbbf902caafdd5c6270a
nopparat231 <23.noop@gmail.com>
2021-01-06 00:48:05 +0700

    add api covid-19

---
+++ src/antds/AntHome.js
@@ -0,0 +1,34 @@
+
+import { Button, DatePicker, version } from "antd";
+import "antd/dist/antd.css";
+import "./Antds.css";
+import { Tabs } from 'antd';
+import MyComponent from "./apiget";
+
+const { TabPane } = Tabs;
+
+function callback(key) {
+  console.log(key);
+}
+
+
+ function Antds() {
+     return(
+        <div className="Appant">
+        <Tabs onChange={callback} type="card">
+        <TabPane tab="Covid-19 In Thai" key="1">
+          <MyComponent />
+        </TabPane>
+        <TabPane tab="Covid-19 In World" key="2">
+          Content of Tab Pane 2
+        </TabPane>
+        <TabPane tab="Tab 3" key="3">
+          Content of Tab Pane 3
+        </TabPane>
+      </Tabs>
+      </div>
+     );
+ }
+
+
+export default Antds;
---
+++ src/antds/apiget.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+
+function Getapi() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    // Note: the empty deps array [] means
+    // this useEffect will run once
+    // similar to componentDidMount()
+    useEffect(() => {
+        fetch("https://covid19.th-stat.com/api/open/today")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result);
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <div>
+                <span>{JSON.stringify(items)}</span>
+            </div>
+        );
+    }
+}
+
+export default Getapi;
